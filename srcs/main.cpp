1263696f1c3d31bc1472a0951d7c154ebbab67bc
Delphine Bouron <dbouron@student.42lyon.fr>
2023-01-30 16:49:32 +0100

    Preparing for merge start_and_run branch

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -15,23 +15,32 @@
 int main(int argc, char *argv[])
 {
 	int	port;
+	char *err;
 
 	if (argc != 3)
+	{
 		std::cout << "Usage: ./ft_irc [port] [password]" << std::endl;
-	port = std::atoi(argv[1]);
-	if (port < 0 || port > 65535)
-		std::cout << "Port range must be between 0 and 65535" << std::endl;
-	Server	server(port, argv[2]);
+		return (1);
+	}
 
+	port = (int)std::strtol(argv[1], &err, 10);
+	if (*err) { /* handle error */ } //TODO handle error
+	if (port < 0 || port > 65535)
+	{
+		std::cout << "Port range must be between 0 and 65535" << std::endl;
+		return (1);
+	}
+
+	Server	server(port, argv[2]);
 	if (!server.starting())
 	{
 		std::cout << "" << std::endl;
-		return (-1);
+		return (1);
 	}
 	if (!server.run())
 	{
 		std::cout << "" << std::endl;
-		return (-1);
+		return (1);
 	}
 	std::cout << "Shutting down the server" << std::endl;
 	return (0);
