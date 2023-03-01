2293df553736672a0821d6f3289fb728aa1e8110
pierrot <saulnier254@gmail.com>
2023-03-01 12:25:02 +0100

    bot: working but need somes fixes

--- bot/srcs/Bot.cpp
+++ bot/srcs/Bot.cpp
@@ -6,7 +6,7 @@
 /*   By: psaulnie <psaulnie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 11:10:46 by psaulnie          #+#    #+#             */
-/*   Updated: 2023/02/27 20:35:40 by psaulnie         ###   ########.fr       */
+/*   Updated: 2023/03/01 12:21:58 by psaulnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,9 @@ void	Bot::shutdown()
 void	Bot::run()
 {
 	int			rvalue;
+	fd_set		read_fd_set;
 	std::string	output;
+
 
 	receive(output, _socketFd);
 	
@@ -83,25 +85,24 @@ void	Bot::run()
 	output.clear();
 	while (true)
 	{
+		// FD_ZERO(&read_fd_set); //  Cleaning the FD list before filling it
+		// FD_SET(_socketFd, &read_fd_set);
+		// rvalue = select(FD_SETSIZE, &read_fd_set, NULL, NULL, NULL);
+		// if (rvalue < 0)
+		// {
+		// 	std::perror("select");
+		// 	throw std::exception();
+		// }
 		receive(output, _socketFd);
-		std::cout << output << std::endl;
-		if (output.find(":MysteryIc JOIN ") != std::string::npos)
+		if (output == "")
 		{
-			std::cout << "sal" << std::endl;
-			_currChannel = _possibleMsg;
-			emit(":MysteryIc PRIVMSG " + _currChannel + " Bot will send messages here!", _socketFd);
-		}	// receive(output, _socketFd); // TODO only use one recv, put it in handle()
-	// std::cout << ":MysteryIc JOIN " + chanName << std::endl;
-	// std::cout << output.find(":MysteryIc JOIN " + chanName) << std::endl;
-	// if (output.find(":MysteryIc JOIN " + chanName) != std::string::npos)
-	// {
-	// 	std::cout << "sal" << std::endl;
-	// 	_currChannel = chanName;
-	// 	emit(":MysteryIc PRIVMSG " + _currChannel + " Bot will send messages here!", _socketFd);
-	// }
+			std::cout << "error: connection lost" << std::endl;
+			std::exit(1);
+		}
 		handle(output);
 		if (_currChannel != "")
 			check();
+		std::cout << "a" << std::endl;
 		output.clear();
 	}
 }
@@ -111,6 +112,11 @@ void	Bot::handle(std::string const &output)
 	std::vector<std::string>	parsed_output;
 	std::string					tmp;
 
+	if (output.find(":MysteryIc JOIN ") != std::string::npos)
+	{
+		_currChannel = _possibleMsg;
+		emit("PRIVMSG " + _currChannel + " :Bot will send messages here!", _socketFd);
+	}
 	for (size_t i = 0; i < output.length(); i++)
 	{
 		char c = output[i];
@@ -134,7 +140,6 @@ void	Bot::emit(std::string const &input, int const &fd) const
 	std::string	msg = input + "\r\n";
 	int 		error;
 
-	std::cout << "Message sent: " << input << std::endl;
 	error = send(fd, msg.c_str(), msg.size(), 0);
 	if (error < 0)
 	{
@@ -172,12 +177,12 @@ int	Bot::receive(std::string &output, int const &fd)
 void	Bot::setMsg(std::vector<std::string> &input)
 {
 	std::string::const_iterator msg_it;
-	std::string	user = input[0].substr(0, 1);
+	std::string	user = input[0].substr(1, input[0].size());
 	std::string	who = input[3];
 	std::string	new_msg;
 
 	std::vector<std::string>::iterator it = input.begin();
-	it++; it++; it++;
+	it++; it++; it++; it++;
 
 	while (it != input.end())
 	{
@@ -186,40 +191,47 @@ void	Bot::setMsg(std::vector<std::string> &input)
 			new_msg.append(" ");
 	}
 	if (new_msg == "")
-		who = "";
+		who = ":HELP";
     for (msg_it = new_msg.begin(); msg_it != new_msg.end(); ++msg_it)
-    {
         if (!std::isspace(*msg_it))
 			break ;
-    }
+
 	if (msg_it == new_msg.end())
 		who = "";
 	if (who == ":SCOOBY" || who == ":SCOOB" || who == ":SCOOBY-DOO")
+	{
 		_scoob_msg = new_msg;
+		emit("PRIVMSG " + user + " " + who.substr(1, who.length() - 1) + " will now say: " + "\"" + new_msg + "\"", _socketFd);
+	}
 	else if (who == ":VELMA")
+	{
 		_velma_msg = new_msg;
+		emit("PRIVMSG " + user + " " + who.substr(1, who.length() - 1) + " will now say: " + "\"" + new_msg + "\"", _socketFd);
+	}
 	else if (who == ":SHAGGY")
+	{
 		_shaggy_msg = new_msg;
+		emit("PRIVMSG " + user + " " + who.substr(1, who.length() - 1) + " will now say: " + "\"" + new_msg + "\"", _socketFd);
+	}
 	else if (who == ":FRED")
+	{
 		_fred_msg = new_msg;
+		emit("PRIVMSG " + user + " " + who.substr(1, who.length() - 1) + " will now say: " + "\"" + new_msg + "\"", _socketFd);
+	}
 	else if (who == ":DAPHNE")
+	{
 		_daphne_msg = new_msg;
+		emit("PRIVMSG " + user + " " + who.substr(1, who.length() - 1) + " will now say: " + "\"" + new_msg + "\"", _socketFd);
+	}
 	else if (who == ":CHANNEL" && input[4] != "")
-	{
 		setChannel(input[4]);
-		return ;
-	}
-	else
-	{
-		emit(":MysteryIc PRIVMSG " + user + " Usage => /msg MysteryIc [SCOOBY, VELMA, SHAGGY, DAPHNE, FRED, CHANNEL] [message, #channelname] {...} {...}", _socketFd);
-		return ;
-	}
-	emit(":MysteryIc PRIVMSG " + user + " " + who.substr(1, who.length() - 1) + " will now say: " + "\"" + new_msg + "\"", _socketFd);
+	else if (who == ":HELP")
+		emit("PRIVMSG " + user + " Usage => /msg MysteryIc [SCOOBY, VELMA, SHAGGY, DAPHNE, FRED, CHANNEL] [message, #channelname] {...} {...}", _socketFd);
 }
 
 void	Bot::check()
 {
-	if (std::difftime(std::time(NULL), _curr_time) >= 5)
+	if (std::difftime(std::time(NULL), _curr_time) >= 300)
 	{
 		sendMsg();
 		_curr_time = std::time(NULL);
@@ -230,39 +242,16 @@ void	Bot::sendMsg()
 {
 	if (_currChannel == "")
 		return ;
-	emit(":MysteryIc PRIVMSG " + _currChannel + " " + _announce_msg, _socketFd);
-	emit(":SCOOBY-DOO PRIVMSG " + _currChannel + " " + _scoob_msg, _socketFd);
-	emit(":VELMA PRIVMSG " + _currChannel + " " + _velma_msg, _socketFd);
-	emit(":SHAGGY PRIVMSG " + _currChannel + " " + _shaggy_msg, _socketFd);
-	emit(":DAPHNE PRIVMSG " + _currChannel + " " + _daphne_msg, _socketFd);
-	emit(":FRED PRIVMSG " + _currChannel + " " + _fred_msg, _socketFd);
+	emit("NOTICE " + _currChannel + " :" + _announce_msg, _socketFd);
+	emit("PRIVMSG " + _currChannel + " SCOOBY-DOO: " + _scoob_msg, _socketFd);
+	emit("PRIVMSG " + _currChannel + " VELMA: " + _velma_msg, _socketFd);
+	emit("PRIVMSG " + _currChannel + " SHAGGY: " + _shaggy_msg, _socketFd);
+	emit("PRIVMSG " + _currChannel + " DAPHNE: " + _daphne_msg, _socketFd);
+	emit("PRIVMSG " + _currChannel + " FRED: " + _fred_msg, _socketFd);
 }
 
 void	Bot::setChannel(std::string const &chanName)
 {
-	std::string	output;
-	// TOCHECK for checking if a chan operator emit NAMES command
 	emit("JOIN " + chanName, _socketFd);
 	_possibleMsg = chanName;
-	// receive(output, _socketFd); // TODO only use one recv, put it in handle()
-	// std::cout << ":MysteryIc JOIN " + chanName << std::endl;
-	// std::cout << output.find(":MysteryIc JOIN " + chanName) << std::endl;
-	// if (output.find(":MysteryIc JOIN " + chanName) != std::string::npos)
-	// {
-	// 	std::cout << "sal" << std::endl;
-	// 	_currChannel = chanName;
-	// 	emit(":MysteryIc PRIVMSG " + _currChannel + " Bot will send messages here!", _socketFd);
-	// }
-	// emit JOIN with the channel
-	// for (it = _channels.begin(); it != _channels.end(); it++)
-	// {
-	// 	std::cout << chanName << std::endl;
-	// 	if (chanName == _currChannel)
-	// 		break ;
-	// }
-	// if (it == _channels.end())
-	// {
-	// 	emit(":MysteryIc PRIVMSG " + _currChannel + " There is no such channel!", _socketFd);
-	// 	return ;
-	// }
 }
